95c60f2002efed61253e271f6e703d958671b87f
유정인 <124610396+jaylenyu@users.noreply.github.com>
2023-07-29 14:44:11 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,10 +1,8 @@
 const solution = (rsp) => {
-    let result = [];
-    let arr = rsp.split('')
-    for (let i = 0; i < rsp.length; i++) {
-        if (arr[i] === '0') result.push('5')
-        if (arr[i] === '2') result.push('0')
-        if (arr[i] === '5') result.push('2')
+    let arr = {
+        0:5,
+        2:0,
+        5:2
     }
-    return result.join('')
+    return [...rsp].map((el) => arr[el]).join('')
 }
