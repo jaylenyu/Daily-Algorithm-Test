a90e6999296eb395302fa15ec6eff4ea759df91c
유정인 <124610396+jaylenyu@users.noreply.github.com>
2023-08-22 18:03:28 +0900

    [unrated] Title: 할 일 목록, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.js
@@ -1,11 +1,3 @@
 const solution = (list, finished) => {
-    let result = [];
-    for (let i = 0; i < list.length; i++) {
-        for (let j = 0; j < finished.length; j++) {
-            if (i === j && finished[j] === false) {
-                result.push(list[i])                
-            }
-        }
-    }
-    return result
+    return list.filter((el, idx) => !finished[idx])
 }
