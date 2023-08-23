99e8d0ed6b43cd99e7f80216d100a4e832b3dba0
유정인 <124610396+jaylenyu@users.noreply.github.com>
2023-08-23 18:54:24 +0900

    [level 0] Title: 약수 구하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,9 @@
+const solution = (n) => {
+    let result = [];
+    for (let i = 1; i < n + 1; i++) {
+        if (n % i === 0) {
+            result.push(i)
+        }
+    }
+    return result
+}
